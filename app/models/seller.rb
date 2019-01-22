879c0ead81e0fefa2ce6db4c77823f905a881bb0
lolaodelola <damzcodes@gmail.com>
2019-01-22 14:56:54 +0000

    change price to amount

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,7 +1,7 @@
 class Seller < ApplicationRecord
   has_many :products
 
-  def add_balance(price)
-    update!(total_balance: total_balance + price, current_balance: current_balance + price)
+  def add_balance(amount)
+    update!(total_balance: total_balance + amount, current_balance: current_balance + amount)
   end
 end
