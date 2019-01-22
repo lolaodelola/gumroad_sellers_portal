234a9e08998d1b35646aceebe06e0475ad1c0972
lolaodelola <damzcodes@gmail.com>
2019-01-22 14:01:47 +0000

    add balance after purchase is created

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,11 @@
 class Purchase < ApplicationRecord
   belongs_to :product
 
+  after_create :update_seller_balance
+
+  private
+  def update_seller_balance
+    seller = product.seller
+    seller.add_balance(price)
+  end
 end
