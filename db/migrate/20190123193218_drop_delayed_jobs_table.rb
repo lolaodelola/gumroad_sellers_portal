3d5144b7e7fa8ab14ae7c33d80b565e2371a0365
lolaodelola <damzcodes@gmail.com>
2019-01-23 19:36:43 +0000

    drop delayed jobs table

---
+++ db/migrate/20190123193218_drop_delayed_jobs_table.rb
@@ -0,0 +1,16 @@
+class DropDelayedJobsTable < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :delayed_jobs do |t|
+      t.integer "priority", default: 0, null: false
+      t.integer "attempts", default: 0, null: false
+      t.text "handler", null: false
+      t.text "last_error"
+      t.datetime "run_at"
+      t.datetime "locked_at"
+      t.datetime "failed_at"
+      t.string "locked_by"
+      t.string "queue"
+      t.timestamps null: false
+    end
+  end
+end
